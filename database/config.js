2697648fc922474d57ff710057366de3567bd5de
Jose Vicente Berna Martinez <jvberna@ua.es>
2020-07-31 08:32:58 +0200

    Versión inicial con comentarios

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect(process.env.DB_CON, {
+        await mongoose.connect(process.env.DB_CONLOCAL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-require('dotenv').config()
-
+// Incluimos el módulo dotenv que carga variables de entorno (el archivo .env)
+// automáticamente el método config() busca un archivo .env y carga las variables de entorno
+require('dotenv').config();
+// Incluimos express
 const express = require('express');
+// Incluimos CORS
 const cors = require('cors')
-
+    // Inclumos el archivo config.js que se encarga de configurar la conexión a BD
 const { dbConnection } = require('./database/config')
 
 // crear el servidor expres
 const app = express();
 
-//app.use(cors())
-
+// Añadimos CORS a express
+app.use(cors())
+    // Conexión con BD
 dbConnection();
 
 
-console.log(process.env);
-
-
+/* 
+Ejemplo de ruta / que devuelve un JSON con OK y Hola Mundo
+Ruta --> '/'
+Controlador --> (req, res) => {
+                res.json({
+                    ok: true,
+                    msg: 'Hola mundo'
+                })
+*/
 app.get('/', (req, res) => {
-
     res.json({
         ok: true,
         msg: 'Hola mundo'
     });
-
-
 });
 
+/*
+    Abrimos el servidor para escuchar las rutas cargadas
+*/
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
 });
