8b3ed35df44694185f8dbf4d13add583f1ddb0eb
Jose Vicente Berna Martinez <jvberna@ua.es>
2020-08-01 21:38:20 +0200

    Cifrado de la contraseña con bcryptjs

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,6 +2,10 @@
 Importamos response para poder usarlo y tipar la res de cualquier ruta, así podemos usar los métodos en VSC cuando escribimos res.
 */
 const { response } = require('express');
+/*
+Importamos librería de cifrado
+*/
+const bcrypt = require('bcryptjs');
 
 /*
 Importacioón del modelo Usuarios, se pone con la U mayuscula (recomendación) porque es una clase ya que tiene argumentos y métodos
@@ -25,17 +29,9 @@ const getUsuarios = async(req, res) => {
 
 // post /
 const crearUsuarios = async(req, res = response) => {
-    // extraermos de req.body los valores enviados
-    const { nombre, apellidos, email, password } = req.body;
 
-
-
-    if (!nombre) {
-        return res.status(400).json({
-            ok: false,
-            msg: 'Falta el nombre'
-        });
-    }
+    // extraermos de req.body los valores que necesitamos comprobar especialmente en código
+    const { email, password } = req.body;
 
     // try/cach para capturar errores
     try {
@@ -52,8 +48,15 @@ const crearUsuarios = async(req, res = response) => {
         // creamos una instancia de la clase Usuario y le pasamos el req.body, y va a coger del req.body los campos que se llamen igual que los argumentos del modelo
         // si viene algún campo que en el model no está delcarado lo ignora
         const usuario = new Usuario(req.body);
+
+        // encriptar contraseña
+        const salt = bcrypt.genSaltSync(); // generamos un salt, una cadena aleatoria
+        usuario.password = bcrypt.hashSync(password, salt); // y aquí ciframos la contraseña
+
+
         // Guardamos en la BD, y devuleve una promesa y hay que espera a que termine para devolver el resultado, por lo que ponemos await delante
         // pana poner await hay que declarar la función crearUsuaruios como async
+
         await usuario.save();
 
         res.json({
